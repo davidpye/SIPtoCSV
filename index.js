60bce7c86023d242f02f8375b7a11a7920e9572b
davidpye <dpyedpye@gmail.com>
2020-06-26 15:01:11 +0100

    Test Split by newline

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function parseInputs() {
   let inputShelfmarks = shelfmarksString
     .replace(/[/]/g, "!2F")
     .replace(/[,]/g, "")
-    .split(/\r|\n/|' ');
+    .split(/\r|\n/);
   parentSlug = document.getElementById("qubitParentSlug").value;
   identifierPrefix = document.getElementById("identifierPrefix").value;
   institutionName = document.getElementById("repository").value;
